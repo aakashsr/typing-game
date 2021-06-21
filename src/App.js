c0522c117e32774f0678ac3e083488acc1a0c838
unknown <aakashsri.dev@gmail.com>
2021-06-21 22:14:55 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React , {useState,useEffect} from 'react'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-
-  const [text,setText] = useState("");
-  const [count,setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [timer, setTimer] = useState(20);
 
   const handleChange = (e) => {
     setText(e.target.value);
-  }
+  };
 
   const handleCount = (e) => {
-    const wordsArr = text.trim().split(' ');
-    const filteredWords = wordsArr.filter((word) => word !== '')
-    console.log(filteredWords,filteredWords.length);
+    const wordsArr = text.trim().split(" ");
+    const filteredWords = wordsArr.filter((word) => word !== "");
+    return filteredWords.length;
+  };
+
+  function tick() {
+    console.log('ticking');
+    setTimer((prevCount) => {
+      if (prevCount !== 0) {
+        return prevCount - 1;
+      } else {
+        return 0;
+      }
+    });
   }
+
+  useEffect(() => {
+    const interval = setTimeout(tick, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   console.log(text);
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
       <textarea value={text} onChange={handleChange}></textarea>
-      <p>Time remaining: 10</p>
+      <p>Time remaining: {timer}</p>
       <button onClick={handleCount}>START</button>
     </div>
   );
