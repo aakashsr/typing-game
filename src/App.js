ac7b52008903bbc935db1ad144531b3565736a3f
unknown <aakashsri.dev@gmail.com>
2021-06-21 21:12:38 +0530

    added handleChange and handleCount methods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+import React , {useState,useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [text,setText] = useState("");
+  const [count,setCount] = useState(0);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const handleCount = (e) => {
+    const wordsArr = text.trim().split(' ');
+    const filteredWords = wordsArr.filter((word) => word !== '')
+    console.log(filteredWords,filteredWords.length);
+  }
+
+  console.log(text);
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
-      <textarea></textarea>
+      <textarea value={text} onChange={handleChange}></textarea>
       <p>Time remaining: 10</p>
-      <button>START</button>
+      <button onClick={handleCount}>START</button>
     </div>
   );
 }
